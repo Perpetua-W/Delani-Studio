4d5952b53b490e6e5c60d6a98727c319ec9eb53d
Perpetua-W <perpetua.gatheru@gmail.com>
2023-04-27 23:46:41 +0300

    code indentation

--- js/scripts.js
+++ js/scripts.js
@@ -51,33 +51,25 @@ $(document).ready(function () {
   });
 });
 
-// Select the form and message input elements
 const form = document.querySelector("#contact-form");
 const messageInput = document.querySelector("#message");
 
-// Listen for the Enter key press event on the message input field
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // Get the user's input values
   const name = form.elements.name.value;
   const email = form.elements.email.value;
   const message = form.elements.message.value;
 
-  // Validate the email address using a regular expression
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(email)) {
-    alert('Please enter a valid email address.');
+    alert("Please enter a valid email address.");
     return;
   }
-  
 
-  // Show a popup message to the user
   alert(
     `${name} We have received your message. Thank you for reaching out to us.`
   );
 
-  // Reset the form field
   form.reset();
-
 });
