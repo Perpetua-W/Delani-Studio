8fe34b1ab748e5526e83a9e1f94ca2a8f0e14f2c
Perpetua-W <perpetua.gatheru@gmail.com>
2023-04-27 10:36:37 +0300

    README file added

---
+++ README.md
@@ -0,0 +1,44 @@
+# Coding Introduction
+#### Akan Name Calculator Site built with HTML, CSS and JavaScript 22/03/2023 #### 
+
+By **Perpetua Wanjiku**
+
+## Description
+This is an application build to output the Akan name of a person, which is issued after calculating the day of the week when one was born after one inputs their birth date, month and year.
+
+## HOW  TO USE
+Requiremets
+     - A computer, Tablet or Phone
+     - Internet access
+
+## View Live Site
+
+To access the application you need to have a browser installed on your machine and access it through URL: https://perpetua-w.github.io/Akan-name-calculator/
+
+## Known Bugs
+No known bags
+
+## Technologies Used
+Languages used to develop the application are 
+
+*HTML - HyperText Markup Languaue that allows to describe web pages
+
+*CSS - Is a style Sheet Languaue that allows to stle a HTML document
+
+*JavaScript - lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat.99
+
+## Support and contact details
+If you have a problem, questions, ideas or suggestions kindly get in touch through perpetua.gatheru@gmail.com
+
+If you need to contribute, kindly fork by pressing the fork button on the top right and clone the repository with this command
+  git clone: https://github.com/Perpetua-W/Akan-name-calculator.git
+
+### License
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Copyright (c) 2023 **Perpetua Wanjiku**
+  
--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,72 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var timeoutId;
   var isDoubleClick = false;
-  
-  $('.icon').on('mousedown', function() {
-    var icon = $(this).find('img');
-    var desc = $(this).find('.description');
+
+  $(".icon").on("mousedown", function () {
+    var icon = $(this).find("img");
+    var desc = $(this).find(".description");
     icon.hide();
     desc.show();
-    
-    timeoutId = setTimeout(function() {
+
+    timeoutId = setTimeout(function () {
       icon.show();
       desc.hide();
-      $(document).off('mouseup');
+      $(document).off("mouseup");
       isDoubleClick = false;
     }, 1000);
-    
-    $(document).on('mouseup', function() {
+
+    $(document).on("mouseup", function () {
       clearTimeout(timeoutId);
       if (!isDoubleClick) {
         icon.show();
         desc.hide();
-        $(document).off('mouseup');
+        $(document).off("mouseup");
       }
     });
   });
-  
-  $('.icon').on('dblclick', function() {
-    var icon = $(this).find('img');
-    var desc = $(this).find('.description');
+
+  $(".icon").on("dblclick", function () {
+    var icon = $(this).find("img");
+    var desc = $(this).find(".description");
     icon.hide();
     desc.show();
     isDoubleClick = true;
-    
-    $(document).on('mouseup', function() {
+
+    $(document).on("mouseup", function () {
       clearTimeout(timeoutId);
       isDoubleClick = false;
     });
   });
 });
 
-$(document).ready(function() {
-  $('.thumbnail').each(function() {
-    var projectName = $(this).find('.project-name').text();
-    $(this).find('.overlay').append('<div class="project-name">' + "" + '</div>');
+$(document).ready(function () {
+  $(".thumbnail").each(function () {
+    var projectName = $(this).find(".project-name").text();
+    $(this)
+      .find(".overlay")
+      .append('<div class="project-name">' + "" + "</div>");
   });
 });
 
-
-
 // Select the form and message input elements
-const form = document.querySelector('#contact-form');
-const messageInput = document.querySelector('#message');
+const form = document.querySelector("#contact-form");
+const messageInput = document.querySelector("#message");
 
 // Listen for the Enter key press event on the message input field
-form.addEventListener('submit', (event) => {
- 
-    event.preventDefault();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    // Get the user's input values
-    const name = form.elements.name.value;
-    const email = form.elements.email.value;
-    const message = form.elements.message.value;
+  // Get the user's input values
+  const name = form.elements.name.value;
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
 
-    // Show a popup message to the user
-    alert(`${name} We have received your message. Thank you for reaching out to us.`);
-
-    // Reset the form field
   
+
+  // Show a popup message to the user
+  alert(
+    `${name} We have received your message. Thank you for reaching out to us.`
+  );
+
+  // Reset the form field
 });
